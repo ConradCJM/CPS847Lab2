84a70dfe9d4e7b41e96a03f71197eb2855b5bd36
ConradCjM <cjconradjoshmejia@gmail.com>
2025-09-24 21:08:32 -0400

    updating pythonfile

--- pythonfile.py
+++ pythonfile.py
@@ -1,2 +1,4 @@
-print("working on source code using Git & GitHub");
-print("code sample for A3 in Python");
+print("working on source code using Git & GitHub")
+print("code sample for A3 in Python")
+print("adding more code to the file")
+print("finalizing the code")
